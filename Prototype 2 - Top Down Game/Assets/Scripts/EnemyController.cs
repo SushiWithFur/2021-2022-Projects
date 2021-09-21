ccf814915ecd88f7c417540c1205ad0cc341ede0
Cooper <cooper.c757@stu.nebo.edu>
2021-09-21 14:17:47 -0600

    Added more movement options and Bounds

---
+++ Prototype 2 - Top Down Game/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float xRange = 9.9f;
+    public float yRange = 4.35f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //keeps enemy in bounds
+        
+        //right wall
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //left wall
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        //top wall
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+        //bottom wall
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }    
+    }
+}
--- Prototype 2 - Top Down Game/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Top Down Game/Assets/Scripts/PlayerController.cs
@@ -9,14 +9,42 @@ public class PlayerController : MonoBehaviour
     public float hInput;
     public float vInput;
 
+    public float xRange = 10.53f;
+    public float yRange = 5.00f;
+
     // Update is called once per frame
     void Update()
     {
+        //gathering keyboard input for movement
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
-
+        // Makes Player rotate left and right
         transform.Translate(Vector3.right * speed * Time.deltaTime * hInput);
+        // Makes Player move forward and back
         transform.Translate(Vector3.up * speed * Time.deltaTime *vInput);
+
+        //keeps player in bounds
         
+        //right wall
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //left wall
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //top wall
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+        //bottom wall
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
     }
 }
