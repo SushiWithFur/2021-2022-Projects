72199a490e2926efdc8c673f357cae385c1f0142
Cooper <cooper.c757@stu.nebo.edu>
2021-10-28 14:17:28 -0600

    Worked on Enemy and Enemy AI

---
+++ Prototype 3 - First Person Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Stats")]
+    public int curHP;
+    public int maxHP;
+    public int scoreToGive;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float attackRange;
+    public float yPathOffset;
+    
+    private List<Vector3> path;
+
+    private Weapon weapon;
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gather Components
+        weapon = GetComponent<Weapon>();
+        target = FindObjectOfType<PlayerController>().gameObject;
+
+        InvokeRepeating("UpdatePath", 0.0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        //Calculate a path to the target
+        NavMeshPath navMeshPath = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, navMeshPath);
+
+        //Save path as a list
+        path = navMeshPath.corners.ToList();
+    }
+
+    void ChaseTarget()
+    {
+        if(path.Count == 0)
+            return;
+
+        //move towards the closest path
+        transform.position = Vector3.MoveTowards(transform.position, path[0] + new Vector3(0, yPathOffset, 0), moveSpeed * Time.deltaTime);
+
+        if(transform.position == path[0] + new Vector3(0, yPathOffset, 0))
+            path.RemoveAt(0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
